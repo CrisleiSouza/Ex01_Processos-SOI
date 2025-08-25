7d78932eba078cb0e5b12a6118c6e4c19755f4a6
crislei-vm <crislei-vm@crisleivm-VirtualBox>
2025-08-24 22:06:29 -0300

    Atualização

--- src/controller/RedesController.java
+++ src/controller/RedesController.java
@@ -15,7 +15,6 @@ public class RedesController {
 	
 	public void ip() {
 		String[] comando = new String[1];
-		String adaptador = "";
 		
 		if (os().contains("Windows")) { // Para Windows:
 			try {
@@ -26,8 +25,10 @@ public class RedesController {
 				InputStreamReader leitor = new InputStreamReader(stream);
 				BufferedReader ler = new BufferedReader(leitor);
 				String linha = ler.readLine();
+				String adaptador = "";
 				
 				while (linha != null) {
+					
 					if (linha.contains("Adaptador")) { // Para armazenar o nome do adaptador
 						adaptador = linha;
 					}
@@ -35,7 +36,7 @@ public class RedesController {
 						String[] ipv4 = linha.split(":");
 						ipv4[1] = (ipv4[1].trim());
 						
-						System.out.println(adaptador + " " + ipv4[1]);
+						System.out.println(adaptador + " IPv4 - " + ipv4[1]);
 					}
 					linha = ler.readLine();
 				}
@@ -48,9 +49,37 @@ public class RedesController {
 			}
 		}
 		else { // Para Linux:
-			System.out.println("teste linux");
+			comando[0] = "ifconfig";
 			
-			
+			try {
+				Process p = Runtime.getRuntime().exec(comando);
+				
+				InputStream stream = p.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(stream);
+				BufferedReader ler = new BufferedReader(leitor);
+				String linha = ler.readLine();
+				String[] adaptador = new String[2];
+				
+				while (linha != null) {
+					if (linha.contains("flags")) {
+						adaptador = linha.split(" ");
+					}
+					if (linha.contains("inet ")) {
+						String[] ipv4 = linha.split("netmask");
+						ipv4 = ipv4[0].split("inet");
+						
+						System.out.println("Adaptador " + adaptador[0] + " inet - " + ipv4[1].trim());
+					}
+					linha = ler.readLine();
+				}
+				
+				ler.close();
+				leitor.close();
+				stream.close();
+				
+			} catch(Exception e) {
+				System.err.println(e.getMessage());
+			}
 		}
 	}
 	
@@ -84,7 +113,26 @@ public class RedesController {
 		else { // Para Linux:
 			String[] comando = "ping -4 -c 10 www.google.com.br".split(" ");
 			
-			
+			try {
+				Process p = Runtime.getRuntime().exec(comando);
+				
+				InputStream stream = p.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(stream);
+				BufferedReader ler = new BufferedReader(leitor);
+				String linha = ler.readLine();
+				
+				while (linha != null) {
+					if (linha.contains("avg")) {
+						String[] media = linha.split("=");
+						media = media[1].split("/");
+						
+						System.out.println("Tempo medio do ping: " + media[1] + " ms");
+					}
+					linha = ler.readLine();
+				}
+			} catch(Exception e) {
+				System.err.println(e.getMessage());
+			}
 		}
 	}
 }
